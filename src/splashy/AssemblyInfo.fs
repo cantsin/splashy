c8491e178d5df78d8a470258a6ed7a6335c688f8
James Tranovich <jtranovich@gmail.com>
2015-04-23 14:20:16 -0700

    Initial revision.

---
+++ src/splashy/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("splashy")>]
+[<assembly: AssemblyProductAttribute("splashy")>]
+[<assembly: AssemblyDescriptionAttribute("A fluid simulator.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
