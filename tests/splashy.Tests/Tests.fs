8b183d81a97aeea314d1c1532bc184dce9199171
James Tranovich <jtranovich@gmail.com>
2015-05-21 18:41:31 -0700

    Added a divergence test.

--- tests/splashy.Tests/Tests.fs
+++ tests/splashy.Tests/Tests.fs
@@ -2,9 +2,21 @@ module splashy.Tests
 
 open splashy
 open NUnit.Framework
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+
+open Coord
+open Vector
+open Cell
+
+let air = { media = Air; pressure = None; velocity = Vector3d.ZERO }
 
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``divergence of standalone cell`` () =
+  // setup.
+  let coord = { x = 0<m>; y = 0<m>; z = 0<m>; }
+  let c = (coord, { air with velocity = Vector3d(1.0<m/s>, 1.0<m/s>, 1.0<m/s>) })
+  let n = Seq.map (fun (_, n) -> (n, air)) (coord.neighbors ()) |> Seq.toList
+  Grid.add_cells (c :: n)
+  // func.
+  let d = Pressure.divergence coord
+  Assert.AreEqual(d, -3.0<m/s>)
