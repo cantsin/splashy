80966d49b100ad19bb79072a66a1038111310869
James Tranovich <jtranovich@gmail.com>
2015-06-04 20:55:16 -0700

    Stable simulation with standalone fluid.

--- src/splashy/Pressure.fs
+++ src/splashy/Pressure.fs
@@ -14,7 +14,12 @@ module Pressure =
   let divergence (where: Coord) =
     let cell = Grid.raw_get where
     let is_nonsolid (_, n) = let c = Grid.raw_get n in c.is_not_solid ()
-    let outgoing = cell.velocity
+    let outgoing = where.forward_neighbors ()
+                   |> Seq.filter is_nonsolid
+                   |> Seq.fold (fun accum (dir, n) ->
+                                  let v = Coord.border dir cell.velocity
+                                  accum .+ v
+                               ) Vector3d.ZERO
     let incoming = where.backward_neighbors ()
                    |> Seq.filter is_nonsolid
                    |> Seq.fold (fun accum (dir, n) ->
@@ -69,17 +74,20 @@ module Pressure =
     Seq.zip markers pressure_units
 
   // calculate solid wall pressure (which is not uniform!)
-  let get_solid_pressure (where: Coord) (origin: Coord) p (inv_c: float<(m^2*s)/kg>) d =
-    let vsolid = (Grid.raw_get where).velocity
+  let get_solid_pressure solid_cell (origin: Coord) p (inv_c: float<(m^2*s)/kg>) d =
+    let vsolid = solid_cell.velocity
     let vborder = (Grid.raw_get origin).velocity
-    let (diff: float<m/s>) = (Coord.border_value d vborder) - (Coord.border_value d vsolid)
+    let (diff: float<m/s>) =
+      match d with
+        | NegX | NegY | NegZ -> (Coord.border_value d vborder) - (Coord.border_value d vsolid)
+        | PosX | PosY | PosZ -> (Coord.border_value d vsolid) - (Coord.border_value d vborder)
     let (result: float<kg/(m*s^2)>) = p + (diff / inv_c)
     result
 
   let get_pressure (origin: Coord) p inv_c d (where: Coord) =
     match Grid.get where with
       | Some c when c.is_solid () ->
-        get_solid_pressure where origin p inv_c d
+        get_solid_pressure c origin p inv_c d
       | Some c when c.media = Fluid ->
         Option.get c.pressure
       | _ ->
@@ -91,34 +99,30 @@ module Pressure =
     let p3 = where.get_neighbor PosZ |> get_pressure where p inv_c PosZ
     Vector3d<kg/(m*s^2)>(p1 - p, p2 - p, p3 - p)
 
-  // TODO: add support for solids
   let get_adjusted_velocity (where: Coord) p inv_c dir =
     let n = where.get_neighbor dir
-    let v = match Grid.get n with
-              | Some c when c.is_not_solid() ->
-                let pressure = c.pressure |> Option.get
-                let offset = (p - pressure) * inv_c
-                let vel = match dir with
-                            | NegX -> Vector3d<m/s>(offset, 0.0<_>, 0.0<_>)
-                            | NegY -> Vector3d<m/s>(0.0<_>, offset, 0.0<_>)
-                            | NegZ -> Vector3d<m/s>(0.0<_>, 0.0<_>, offset)
-                            | _ -> failwith "Invalid direction."
-                c.velocity .- vel
-              | _ ->
-                Vector3d<m/s>.ZERO
+    let c = Grid.raw_get n
+    let pressure = get_pressure where p inv_c dir n
+    let offset = (p - pressure) * inv_c
+    let vel = match dir with
+                | NegX -> Vector3d<m/s>(offset, 0.0<_>, 0.0<_>)
+                | NegY -> Vector3d<m/s>(0.0<_>, offset, 0.0<_>)
+                | NegZ -> Vector3d<m/s>(0.0<_>, 0.0<_>, offset)
+                | _ -> failwith "Invalid direction."
+    let v = c.velocity .- vel
     (n, v)
 
   // set the pressure such that the divergence throughout the fluid is zero.
   let apply markers dt =
     let inv_c = dt / (Constants.h * Constants.fluid_density)
     Seq.map (fun (m: Coord) ->
-               // negative gradients
+               // positive gradients
                let c = Grid.raw_get m
                let p = c.pressure |> Option.get
                let gradient = forwards_gradient m p inv_c
                let offset = gradient .* inv_c
                let v = c.velocity .- offset
-               // positive gradients
+               // negative gradients
                let nvs = [get_adjusted_velocity m p inv_c NegX;
                           get_adjusted_velocity m p inv_c NegY;
                           get_adjusted_velocity m p inv_c NegZ]
