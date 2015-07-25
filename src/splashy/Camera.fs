dd7693fdcf92fb7c844083488115187c2585439a
James Tranovich <jtranovich@gmail.com>
2015-07-25 09:27:10 -0700

    Added mouse speed.

--- src/splashy/Camera.fs
+++ src/splashy/Camera.fs
@@ -5,7 +5,8 @@ open OpenTK
 module Camera =
 
   let eye = Vector3(0.0f, 0.0f, -400.0f)
-  let speed = 16.0f
+  let speed = 32.0f
+  let mouseSpeed = 4.0f
 
   type Camera () =
     let mutable m = Matrix4.Identity
@@ -17,7 +18,7 @@ module Camera =
       m <- m * Matrix4.CreateTranslation(v * speed)
 
     member this.rotate yaw pitch =
-      m <- m * Matrix4.CreateRotationY(yaw)
-      m <- m * Matrix4.CreateRotationX(pitch)
+      m <- m * Matrix4.CreateRotationY(yaw * mouseSpeed)
+      m <- m * Matrix4.CreateRotationX(pitch * mouseSpeed)
 
     member this.matrix () = m
